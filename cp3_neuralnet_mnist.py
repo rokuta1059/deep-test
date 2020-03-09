d5efd77bba22a166df9a1cd3c2ba43e52a3e788d
Kim woo seob <rokuta1059@gmail.com>
2020-03-10 01:10:05 +0900

    Chapter 3
    신경망

--- cp3_neuralnet_mnist.py
+++ cp3_neuralnet_mnist.py
@@ -5,7 +5,8 @@ import pickle
 from dataset import load_mnist
 from common import sigmoid, softmax
 
-local = "D:\Git_Workspace\deep-test\sample_weight.pkl"
+local = "D:\\Git_Workspace\\deep-test\\sample_weight.pkl"
+
 """
     3.6.2 신경망 추론 처리
     입력층 뉴런: 784개
@@ -14,11 +15,16 @@ local = "D:\Git_Workspace\deep-test\sample_weight.pkl"
     은닉층: 총 2개 (뉴런 수 등은 임의)
     1번째 은닉층: 50개 뉴런
     2번쨰 은닉층: 100개 뉴런
+
+    신경망 각 층의 배열 형상의 추이
+    X     W1         W2         W3         Y
+    784 > 784 x 50 > 50 X 100 > 100 X 10 > 10
 """
 
 def get_data():
+    #(훈련 이미지, 훈련 레이블), (시험 이미지, 시험 레이블)
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
-    return x_test, t_test
+    return x_train, t_train, x_test, t_test
 
 
 def init_network():
@@ -43,14 +49,27 @@ def predict(network, x):
     return y
 
 
-x, t = get_data()
+x_train, t_train, x_test, t_test = get_data()
 network = init_network()
+
+# 훈련 이미지와 레이블에 대한 내용
 accuracy_cnt = 0
-for i in range(len(x)): # 데이터를 한 장씩 읽는다
-    y = predict(network, x[i])  # 한 장씩 분류
-    p= np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
-    if p == t[i]:
+for i in range(len(x_train)):           # 데이터를 한 장씩 읽는다
+    y = predict(network, x_train[i])    # 한 장씩 분류
+    p= np.argmax(y)                     # 확률이 가장 높은 원소의 인덱스를 얻는다.
+    if p == t_train[i]:
         accuracy_cnt += 1
 
 # 정확도 출력
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+print("Accuracy for train:\t" + str(float(accuracy_cnt) / len(x_train)))
+
+# 시험 이미지와 레이블에 대한 내용
+accuracy_cnt = 0
+for i in range(len(x_test)):           # 데이터를 한 장씩 읽는다
+    y = predict(network, x_test[i])    # 한 장씩 분류
+    p= np.argmax(y)                     # 확률이 가장 높은 원소의 인덱스를 얻는다.
+    if p == t_test[i]:
+        accuracy_cnt += 1
+
+# 정확도 출력
+print("Accuracy for test:\t" + str(float(accuracy_cnt) / len(x_test)))
---
+++ cp3_neuralnet_mnist_batch.py
@@ -0,0 +1,57 @@
+import sys, os
+import numpy as np
+import pickle
+from dataset import load_mnist
+from common import sigmoid, softmax
+
+""" 
+    배치: 하나로 묶은 데이터
+    수치 계산 라이브러리 대부분은 큰 배열을 효율적으로 처리할 수 있도록 고도로 최적화 되어있음
+    커다란 신경망은 데이터 전송이 병목이 되는 경우가 자주 있는데, 
+    배치 처리를 통해 버스에 주는 부하를 줄임
+
+    배치 처리를 위한 배열들의 형상 추이
+    X           W1         W2         W3      -> Y
+    100 X 784 > 784 x 50 > 50 X 100 > 100 X 10 > 100 X 10
+"""
+
+local = "D:\\Git_Workspace\\deep-test\\sample_weight.pkl"
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open(local, 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    w1, w2, w3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, w3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100 # 배치 크기
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):  # 0에서부터 len(x)까지 batch_size 간격으로 증가하는 리스트
+    x_batch = x[i:i+batch_size]         # 입력 데이터 묶기
+    y_batch = predict(network, x_batch) # 배치 단위로 묶고 분류
+    p = np.argmax(y_batch, axis=1)      # argmax()는 최대값의 인덱스를 가져옴
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
